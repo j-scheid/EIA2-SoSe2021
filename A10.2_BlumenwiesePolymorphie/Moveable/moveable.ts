914d35ab58827231a4621f93b3a7331ab1b179a9
J-Scheid <jonas@scheid.io>
2021-06-12 15:50:37 +0200

    added A10.2

---
+++ A10.2_BlumenwiesePolymorphie/Moveable/moveable.ts
@@ -0,0 +1,25 @@
+namespace Polymorphie {
+  export class Moveable {
+    xPos: number;
+    yPos: number;
+    velocityX: number;
+    velocityY: number;
+
+    yRandomMin: number;
+    yRandomMax: number;
+
+    constructor(_position: Vector) {
+      this.yPos = _position.y;
+      this.xPos = _position.x;
+    }
+
+    update(): void {
+      if (this.xPos > crc2.canvas.width || this.xPos < 0) {
+        this.velocityX = -this.velocityX;
+      }
+
+      this.xPos += this.velocityX;
+      this.draw();
+    }
+  }
+}
